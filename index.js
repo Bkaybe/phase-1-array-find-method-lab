dded798708c127ca02e8b52028dfe55c845547c5
Bethuel Khisa <sunguribethuel@gmail.com>
2024-07-03 12:51:53 +0300

    Adds the necessary function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+const superbowlWin = (object) => {
+  for (let key in object) {
+    if (object.hasOwnProperty(key) && object[key].result === "W") {
+      return object[key].year;
+    }
+  }
+
+  return undefined;
+};
